91a36033fc44cfa6e1dbae5a1822f2ca36916279
Ryan McKinley <ryantxu@gmail.com>
2021-05-06 21:51:48 -0700

    comment out edge private deps

--- cmd/replay/replay.go
+++ cmd/replay/replay.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	fpath := "/home/ryan/Downloads/influx-sample-data.log"
-	url := "ws://localhost:3000/api/live/push?gf_live_stream=telegraf&frameFormat=labels_column"
+	url := "ws://localhost:3000/api/live/push/telegraf?gf_live_frame_format=labels_column"
 	key := "eyJrIjoicExKYjlEN29yQmlrMEg4YmtodlRFSjN6R0FOUjRLMEQiLCJuIjoicHVibGlzaCIsImlkIjoxfQ=="
 
 	ws := replay.NewWebSocket(url)
